681e5ce26f2682592f9419bab4e3c6402a28fdb5
Arthur Barbosa <arthurbruno03@gmail.com>
2021-06-29 21:58:24 -0400

    created component Login

--- front-end/appointments-ui/src/App.js
+++ front-end/appointments-ui/src/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AppointmentListTable from './components/AppointmentListTable';
 import CustomerForm from './components/CustomerForm';
+import Login from './components/Login';
 import NavBar from './components/NavBar';
 
 class App extends Component {
@@ -16,7 +17,8 @@ class App extends Component {
           <NavBar />
           <div className="container" style={{ marginTop: 20 }}>
             <Switch>
-              <Route path="/form-client" component={CustomerForm} />
+              <Route exact path="/form-client" component={CustomerForm} />
+              <Route exact path="/login" component={Login} />
               <Route path="/" component={AppointmentListTable} />
             </Switch>
           </div>
